6854221d9f07746e11ef6d2c08e1c5a198cbacdf
Devilisch <kirowatanabe@gmail.com>
2019-05-03 01:51:43 +0300

    Add socket_interface and main functions.
    Unstable version.

--- protey_testapp_client.cpp
+++ protey_testapp_client.cpp
@@ -33,8 +33,19 @@
 				- max: 3
 				- min: 1
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #define OFFLINE_STATUS 0
 #define ONLINE_STATUS 1
+#define OFF 0
+#define ON 1
 
 //Интерфэйс работы с сокетами / соединением
 //+Функция создания соединения с сервером по заданным настройкам (с выводом ошибок при некорректно введённых данных)
@@ -54,6 +65,7 @@ public:
 	string socket_type_string = "TCP";
 	string connection_ip = "128.0.0.1"; //Может быть ошибка
 	struct sockaddr_in socket_info_struct;
+	//---
 
 	void set_TCP() { socket_type = SOCK_STREAM; socket_protocol = IPPROTO_TCP; string socket_type_string = "TCP";}
 	void set_UDP() { socket_type = SOCK_DGRAM; socket_protocol = IPPROTO_UDP; string socket_type_string = "UDP";}
@@ -61,7 +73,7 @@ public:
 
 	void create_connection() {
 		socket_info = socket( socket_domain, socket_type, socket_protocol );
-		if( socket_info == -1 ) printf( "ERROR: Socket creation error." );
+		if( socket_info == -1 ) printf( "ERROR: Socket creation error.\n" );
 		else {
 			printf( "Create %s socket.", socket_type_string );
 			memset( &socket_info_struct, 0, sizeof( socket_info_struct ) );
@@ -69,17 +81,17 @@ public:
 			socket_info_struct.sin_port = htons(socket_port);
 			connection_info = inet_pton( socket_domain, connection_ip, &socket_info_struct.sin_addr );
 			if( connection_info < 0 ) {
-				printf( "ERROR: Uncorrect first parameter." );
+				printf( "ERROR: Uncorrect first parameter.\n" );
 				close( socket_info );
 			} else if ( connection_info == 0 ) {
-				printf( "ERROR: Uncorrect second parameter." );
+				printf( "ERROR: Uncorrect second parameter.\n" );
 				close( socket_info );
 			}
 			if( connect( socket_info, (struct sockaddr*)&socket_info_struct, sizeof(socket_info_struct) ) == -1 ) {
-				printf( "ERROR: Connection error." );
+				printf( "ERROR: Connection error.\n" );
 				close( socket_info );
 			} else {
-				printf( "Client connect to the server." );
+				printf( "Client connect to the server.\n" );
 				connection_status = ONLINE_STATUS;
 			}
 		}
@@ -87,21 +99,51 @@ public:
 	}
 
 	void send_message(string message) { //Может быть ошибка
-
+		//Отправка сообщения и приём ответа от сервера
+		//Добавить ограничения на размер сообщения и сделать вывод ошибок
 	}
 
 	void shutdown_connection() {
 		shutdown( socket_info, SHUT_RDWR)
 		close( socket_info );
-		printf( "Client disconnect from the server." );
+		printf( "Client disconnect from the server.\n" );
 		connection_status = OFFLINE_STATUS;
 	}
 }
 
+
+
 //Интерфейс работы с пользвателем
-//Функция обработки входящих команд (с выводом ошибок при их некорректном вводе)
-//Список команд: connect, send, disconnect, send_test_message, send_big_message
+//+Функция обработки входящих команд (с выводом ошибок при их некорректном вводе)
+//+Список команд: connect, send, disconnect, send_test_message, send_big_message
 //-----
 //Функция чтения сообщения с экрана и отправки его на сервер (с выводом сообщения об ошибке в случаях, если размер сообщения превышает 64 кб). 
 //Дополнительно: возможно стоит реализовать отправку больших сообщений с помощью их разделения на несколько.
+class socket_interface : socket_connection {
+public:
+	int programm_status = ON;
+	void new_request(string req) {
+		switch(req){
+			case "-connect": create_connection(); break;
+			case "-TCP": set_TCP(); break;
+			case "-UDP": set_UDP(); break;
+			case "-send": /*send_message();*/ break;
+			case "-send_test1_message": send_message("Hello world! 132\n"); break;
+			case "-disconnect": shutdown_connection(); break;
+			case "-exit": programm_status = OFF; break;
+			default: printf( "Unknown request, try again.\n" );
+		}
+	}
+}
 
+
+
+int main(){
+	socket_interface connection;
+	string request;
+	while( connection.programm_status == ON ) {
+		scanf("%s", request);
+		connection.new_request(request);
+	}
+	return 0;
+}
